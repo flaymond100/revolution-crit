9abfefe3ea7d51216baceaa177941a6fd10c229b
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2026-04-15 12:07:38 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/revolution-crit">
+  <BrowserRouter basename="/">
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 );
